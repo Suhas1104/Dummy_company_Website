698ad9f2b90febc8b43e858fb1b09f657c207366
Suhas1104 <110780058+Suhas1104@users.noreply.github.com>
2023-06-23 15:48:15 +0530

    Add files via upload

--- email.php
+++ email.php
@@ -1,3 +1,24 @@
 <?php
 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "ariesluck172@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+if($email!=NULL){
+    if(mail($to,$subject,$txt)){
+   
+    echo '<script>alert("message sent")</script>';
+    
+    }
+    else
+    echo '<script>alert("failed")</script>';
+
+    header("refresh:2;url=index.html");
+
+}
+
+
 ?>
--- login.php
+++ login.php
@@ -1,7 +1,48 @@
 <html>
 <body>
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "auth";
+$uname=$_POST["username"];
+$pass=$_POST["password"];
+ if (empty($uname)&&empty($pass)){
+    echo '<script>alert("password and username is required")</script>';
+    header("refresh:2;url=login.html");
+}
+else if (empty($uname)) {
 
-Welcome <?php echo $_POST["username"]; ?><br>
+    echo '<script>alert("username is required")</script>';
+    header("refresh:2;url=login.html");
+
+}else if(empty($pass)){
+
+    echo '<script>alert("password is required")</script>';
+    header("refresh:2;url=login.html");
+}
+
+
+
+$db = mysqli_connect($servername, $username, $password, $dbname);
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql="SELECT * FROM cred WHERE email='$uname' AND pass='$pass'";
+$result = $db->query($sql);
+if ($result){
+    if ( $result->num_rows > 0){
+    echo"success";
+}
+else 
+echo"failed";
+}
+else
+echo"failed";
+
+header("refresh:2;url=login.html");
+?>
+
 
 
 </body>
